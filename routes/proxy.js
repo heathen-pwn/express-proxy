89616c92346573019ffd5006c206fdecf9ccbed6
Heathen <amhasan.alizaybawee@gmail.com>
2023-05-29 02:17:48 +0300

    Added HTTPS handler

--- routes/proxy.js
+++ routes/proxy.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const http = require("node:http");
+const https = require("node:https");
 const url = require("url");
 
 /* GET home page. */
@@ -12,7 +13,7 @@ router.post('/', (req, res) => {
   const destinationHost = url.parse(req.body.dhost);
   console.log(destinationHost);
 
-  if (destinationHost.protocol === 'http:') {
+  if (destinationHost.protocol === 'http:') { // HTTP Handling
 
     http.get(destinationHost, (destinationResult) => {
       destinationResult.setEncoding('utf-8');
@@ -35,10 +36,23 @@ router.post('/', (req, res) => {
       })
 
     })
-  } else if (destinationHost.protocol === 'https:') {
-    console.log("https!");
-    // HTTPS
-  } else { // None
+  } 
+  else if (destinationHost.protocol === 'https:') { // HTTPS Handling
+    https.get(destinationHost, (destinationResult) => {
+      destinationResult.setEncoding('utf-8');
+      let rawData = '';
+      destinationResult.on('data', (chunk) => { rawData += chunk });
+      destinationResult.on('end', () => {
+        try {
+          console.log(`Sending... ${rawData}... to POST https request`);
+          res.send(rawData);
+        }
+        catch (e) {
+          console.error(e);
+        }
+      })
+    })
+  } else { // No protocol specified
     // DNS lookup?
   }
 
