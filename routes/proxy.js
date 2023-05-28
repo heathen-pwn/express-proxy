56ac65c816a8aca76dcdba1f1da23290e597b6d7
Heathen <amhasan.alizaybawee@gmail.com>
2023-05-29 01:50:50 +0300

    Added protocol check

--- routes/proxy.js
+++ routes/proxy.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const http = require("node:http");
+const url = require("url");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -8,26 +9,39 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', (req, res) => {
-  const destinationHost = req.body.dhost;
+  const destinationHost = url.parse(req.body.dhost);
   console.log(destinationHost);
-  // Get the website of 'dhost'
-  // Send the body; res.send
-  http.get(destinationHost, (destinationResult) => {
-    destinationResult.setEncoding('utf-8');
-    let rawData = '';
-    destinationResult.on('data', (chunk) => {
-      rawData += chunk;
+
+  if (destinationHost.protocol === 'http:') {
+
+    http.get(destinationHost, (destinationResult) => {
+      destinationResult.setEncoding('utf-8');
+      let rawData = '';
+
+
+
+      destinationResult.on('data', (chunk) => {
+        rawData += chunk;
+      })
+
+      destinationResult.on('end', () => {
+        try {
+          console.log(`Sending... ${rawData}... to POST request`);
+          res.send(rawData);
+        }
+        catch (e) {
+          console.error(e);
+        }
+      })
+
     })
-    destinationResult.on('end', () => {
-      try {
-        console.log(`Sending... ${rawData}... to POST request`);
-        res.send(rawData);
-      }
-      catch (e) {
-        console.error(e);
-      }
-    })
-  })
+  } else if (destinationHost.protocol === 'https:') {
+    console.log("https!");
+    // HTTPS
+  } else { // None
+    // DNS lookup?
+  }
+
 })
 
 module.exports = router;
