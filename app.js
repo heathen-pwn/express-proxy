e3d0e4a3c7cdabaae7583a9182b136ccd7797bc1
Heathen <amhasan.alizaybawee@gmail.com>
2023-05-29 22:33:59 +0300

    Main page

--- app.js
+++ app.js
@@ -20,8 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', proxyRouter);
 app.use('/proxy', proxyRouter);
 
 // catch 404 and forward to error handler
