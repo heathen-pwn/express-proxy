ba2e697f30325116c619522fccc3b4abe52667d7
Heathen <amhasan.alizaybawee@gmail.com>
2023-05-30 00:43:50 +0300

    Added parsing

--- routes/proxy.js
+++ routes/proxy.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const Url = require('url');
 const ProxyAPI = require('../src/models/proxy');
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -7,10 +8,15 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', (req, res) => {
-  console.log(typeof req.body.dhost);
-  const proxy = new ProxyAPI.Proxy(new URL(req.body.dhost), res);
+  // Deprecated
+  // const proxy = new ProxyAPI.Proxy(new URL(req.body.dhost), res);
+  // proxy.get();
+})
+
+router.get('/proxy', (req, res) => {
+  console.log("dhost:", req.query.dhost);
+  const proxy = new ProxyAPI.Proxy(new URL(req.query.dhost), res);
   proxy.get();
-  console.log(proxy);
 })
 
 module.exports = router;
--- src/models/proxy.js
+++ src/models/proxy.js
@@ -1,58 +1,84 @@
-const http = require("node:http");
-const https = require("node:https");
-const url = require("url");
-
+const http = require('node:http');
+const https = require('node:https');
+const cheerio = require('cheerio');
 class Proxy {
     constructor(destination, response) {
         this.destination = destination; // url object or url string
         this.response = response;
     }
-    getHttps() {
-        https.get(this.destination, (res) => {
-            res.setEncoding('utf-8');
-            let rawData = '';
-            res.on('data', (chunk) => { rawData += chunk });
-            res.on('end', () => {
-              try {
-                console.log(`Sending... ${rawData}... to POST https request`);
-                this.response.send(rawData);
-              }
-              catch (e) {
-                console.error(e);
-              }
-            })
-          })  
-    }
-    getHttp() {
-        http.get(this.destination, (res) => {
-            res.setEncoding('utf-8');
-            let rawData = '';
-      
-            res.on('data', (chunk) => {
-              rawData += chunk;
-            })
-      
-            res.on('end', () => {
-              try {
-                console.log(`Sending... ${rawData}... to POST request`);
-                this.response.send(rawData);
-              }
-              catch (e) {
-                console.error(e);
-              }
-            })
-      
-          })
-    }
 
     get() { 
-        if(this.destination.protocol === "http:") {
+        if(this.destination.protocol === 'http:') {
           this.getHttp();
         } else {
-          this.destination.protocol = "https:";
+          this.destination.protocol = 'https:';
           this.getHttps();
         }
     }
+
+    getHttps() {
+      https.get(this.destination, (res) => {
+        res.setEncoding('utf-8');
+        let rawData = '';
+  
+        res.on('data', (chunk) => {
+          rawData += chunk;
+        })
+  
+        res.on('end', () => {
+          try {
+            const parsedData = this.parse(rawData);
+            console.log(`Sending... ${parsedData.slice(0, 250)}... to GET request`);
+            this.response.send(parsedData);
+
+          }
+          catch (e) {
+            console.error(e);
+          }
+        })
+  
+      })
+  }
+  getHttp() {
+      http.get(this.destination, (res) => {
+          res.setEncoding('utf-8');
+          let rawData = '';
+    
+          res.on('data', (chunk) => {
+            rawData += chunk;
+          })
+    
+          res.on('end', () => {
+            try {
+              const parsedData = HTMLParser.parse(rawData);
+              console.log(`Sending... ${parsedData.slice(0, 250)}... to GET request`);
+              this.response.send(parsedData);
+
+            }
+            catch (e) {
+              console.error(e);
+            }
+          })
+    
+        })
+  }
+  parse(raw) {
+    const $ = cheerio.load(raw);
+
+    console.log('href:', $('a').attr('href'));
+    $('a').each((element, index) => {
+      element.attr('href', 'http://localhost/proxy?dhost='+element.attr('href'))
+      console.log(element.attr('href'));
+    })
+
+    // $('a').attr('href', 'http://localhost/proxy?dhost='+this.attr('href'));
+    // $('script').attr('src', 'http://localhost/proxy?dhost='+$('script').attr('src'));
+    // $('img').attr('src', 'http://localhost/proxy?dhost='+$('img').attr('src'));
+    // $('link').attr('href', 'http://localhost/proxy?dhost='+$('link').attr('href'));
+
+    return $.html();
+}
+
 }
 
 module.exports = {
