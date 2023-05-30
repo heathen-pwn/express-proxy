ed3e0abfb097c82d74bf099c77e931f14c0f8a35
Heathen <amhasan.alizaybawee@gmail.com>
2023-05-30 18:43:57 +0300

    Parser

--- src/models/proxy.js
+++ src/models/proxy.js
@@ -1,6 +1,7 @@
 const http = require('node:http');
 const https = require('node:https');
 const cheerio = require('cheerio');
+const { cursorTo } = require('node:readline');
 class Proxy {
     constructor(destination, response) {
         this.destination = destination; // url object or url string
@@ -50,7 +51,7 @@ class Proxy {
     
           res.on('end', () => {
             try {
-              const parsedData = HTMLParser.parse(rawData);
+              const parsedData = this.parse(rawData);
               console.log(`Sending... ${parsedData.slice(0, 250)}... to GET request`);
               this.response.send(parsedData);
 
@@ -62,14 +63,52 @@ class Proxy {
     
         })
   }
+  /*
+    -- Need to add a handler for such events when the href/src/rel is relative
+    dhost: /static/js/src/infer-preferred-language.js?v=b69e09e
+    GET /proxy?dhost=/static/js/src/infer-preferred-language.js?v=b69e09e 500 2.253 ms - 880
+    dhost: undefined
+  */
   parse(raw) {
-    const $ = cheerio.load(raw);
+    const $ = cheerio.load(raw, null, false);
 
     console.log('href:', $('a').attr('href'));
-    $('a').each((element, index) => {
-      element.attr('href', 'http://localhost/proxy?dhost='+element.attr('href'))
-      console.log(element.attr('href'));
-    })
+    $('a').each(handleAnchor);
+    $('script').each(handleScript);
+    $('img').each(handleImage);
+    $('link').each(handleLink);
+
+    function handleAnchor(number, element) {
+      const CurrentElement = $(element);
+      console.log(CurrentElement);
+      const ProxyName = "http://localhost/proxy?dhost=";
+      let newHref = `${ProxyName}${CurrentElement.attr('href')}`;
+      CurrentElement.attr("href", newHref);
+    }
+
+    function handleScript(number, element) {
+      const CurrentElement = $(element);
+      console.log(CurrentElement);
+      const ProxyName = "http://localhost/proxy?dhost=";
+      let newSrc = `${ProxyName}${CurrentElement.attr('src')}`;
+      CurrentElement.attr("src", newSrc);
+    }
+
+    function handleImage(number, element) {
+      const CurrentElement = $(element);
+      console.log(CurrentElement);
+      const ProxyName = "http://localhost/proxy?dhost=";
+      let newSrc = `${ProxyName}${CurrentElement.attr('src')}`;
+      CurrentElement.attr("src", newSrc);
+    }
+
+    function handleLink(number, element) {
+      const CurrentElement = $(element);
+      console.log(CurrentElement);
+      const ProxyName = "http://localhost/proxy?dhost=";
+      let newRel = `${ProxyName}${CurrentElement.attr('rel')}`;
+      CurrentElement.attr("rel", newRel);
+    }
 
     // $('a').attr('href', 'http://localhost/proxy?dhost='+this.attr('href'));
     // $('script').attr('src', 'http://localhost/proxy?dhost='+$('script').attr('src'));
