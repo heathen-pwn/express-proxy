081364b0e943565b688b0dd905c4bcb38377a7f1
Heathen <amhasan.alizaybawee@gmail.com>
2023-05-30 18:44:03 +0300

    Added debugging file

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('<a href="http://icanhazip.com">ICANHAZIP</a>respond with a resource');
+  res.send('<a href="http://icanhazip.com">ICANHAZIP</a>respond with a resource<a href="http://icanhazip.com">ICANHAZIP</a><a href="http://icanhazip.com">ICANHAZIP</a>');
 });
 
 module.exports = router;
